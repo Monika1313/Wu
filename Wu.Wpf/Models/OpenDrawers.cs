09d69312035d238c4fbdca953d7363ba80b9c760
weibin wu <961501261@qq.com>
2022-05-15 00:46:28 +0800

    W

---
+++ Wu.Wpf/Models/OpenDrawers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wu.Wpf.Models
+{
+    public class OpenDrawers
+    {
+
+        ///// <summary>
+        ///// definity
+        ///// </summary>
+        //public bool LeftDrawer { get => _LeftDrawer; set => SetProperty(ref _LeftDrawer, value); }
+        //private bool _LeftDrawer;
+    }
+}
---
+++ Wu/EntityFrameworkCore/UnitOfWork/ReadMe.txt
@@ -0,0 +1,2 @@
+﻿该部分取自 https://github.com/Arch/UnitOfWork
+仅修改了命名空间,方便个人使用
