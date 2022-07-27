7a2508a7a68432082cc7bec8a037922dd259e415
Monika <961501261@qq.com>
2022-07-27 14:47:56 +0800

    W

---
+++ Wu.Wpf/Converters/False2Collapsed.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Wu.Wpf.Converters
+{
+    /// <summary>
+    /// bool转换Visibility false为Collapsed
+    /// </summary>
+    public class False2Collapsed : ValueConverterBase<False2Collapsed>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool b = System.Convert.ToBoolean(value);
+            if (b)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+    }
+}
---
+++ Wu.Wpf/Converters/True2Collapsed.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Wu.Wpf.Converters
+{
+    /// <summary>
+    /// bool转换Visibility false为Collapsed
+    /// </summary>
+    public class True2Collapsed : ValueConverterBase<True2Collapsed>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool b = System.Convert.ToBoolean(value);
+            if (b)
+            {
+                return Visibility.Collapsed;
+            }
+            else
+            {
+                return Visibility.Visible;
+            }
+        }
+    }
+}
