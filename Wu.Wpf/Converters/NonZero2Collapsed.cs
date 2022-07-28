3a5f2a3173f14df99acc27b288dee18cad12042e
weibin wu <961501261@qq.com>
2022-07-28 23:56:26 +0800

    W

---
+++ Wu.Wpf/Converters/NonZero2Collapsed.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wu.Wpf.Converters
+{
+    /// <summary>
+    /// 非零则隐藏
+    /// </summary>
+    public class NonZero2Collapsed : ValueConverterBase<NonZero2Collapsed>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && int.TryParse(value.ToString(), out int result))
+            {
+                if (result.Equals(0))
+                {
+                    return Visibility.Visible;
+                }
+            }
+            return Visibility.Collapsed;
+        }
+    }
+}
--- Wu.Wpf/Converters/Zero2Collapsed.cs
+++ Wu.Wpf/Converters/Zero2Collapsed.cs
@@ -9,9 +9,9 @@ using System.Windows;
 namespace Wu.Wpf.Converters
 {
     /// <summary>
-    /// 小于1则隐藏
+    /// 为0则隐藏
     /// </summary>
-    public class LessThanOne2Collapsed : ValueConverterBase<LessThanOne2Collapsed>
+    public class Zero2Collapsed : ValueConverterBase<Zero2Collapsed>
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
