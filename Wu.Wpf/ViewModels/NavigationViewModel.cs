4ad9ccee627a3e348a9882cbba09ca001b192afe
Monika <961501261@qq.com>
2022-07-29 16:24:44 +0800

    W

---
+++ Wu.Wpf/ViewModels/NavigationViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Prism.Events;
+using Prism.Ioc;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using Wu.Wpf.Events;
+using Wu.Wpf.Extensions;
+
+namespace Wu.CommTool.ViewModels
+{
+    /// <summary>
+    /// 窗口导航类
+    /// </summary>
+    public class NavigationViewModel : BindableBase,INavigationAware
+    {
+        private readonly IContainerProvider provider;
+        public readonly IEventAggregator aggregator;//事件聚合器
+        public NavigationViewModel() { }
+
+        public NavigationViewModel(IContainerProvider provider)
+        {
+            this.provider = provider;
+            this.aggregator = provider.Resolve<IEventAggregator>();
+        }
+
+        /// <summary>
+        /// 是否重用旧窗口
+        /// </summary>
+        /// <param name="navigationContext"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public virtual bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public virtual void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            
+        }
+
+        public virtual void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            
+        }
+
+        /// <summary>
+        /// 打开等待窗口
+        /// </summary>
+        /// <param name="IsOpen"></param>
+        public void UpdateLoading(bool IsOpen)
+        {
+            aggregator.UpdateLoading(new UpdateModel()
+            {
+                IsOpen = IsOpen
+            });
+        }
+    }
+}
