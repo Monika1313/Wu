ce22fa93869b26b810b4126782907145cb9afe19
Monika <961501261@qq.com>
2022-08-11 11:38:09 +0800

    W

--- Wu.Wpf/Common/Utils.cs
+++ Wu.Wpf/Common/Utils.cs
@@ -23,16 +23,25 @@ namespace Wu.Wpf.Common
         /// <summary>
         /// 执行方法 无关UI线程和子线程
         /// </summary>
-        /// <param name="action">需要执行的方法</param>
+        /// <param name="action">执行的方法</param>
         public static void ExecuteFun(Action action)
         {
-            //判断是UI线程还是子线程 若是子线程需要用委托
-            var UiThreadId = Application.Current.Dispatcher.Thread.ManagedThreadId;       //UI线程ID
-            var currentThreadId = Environment.CurrentManagedThreadId;                     //当前线程ID
-            //当前线程为主线程
-            if (currentThreadId == UiThreadId) { action(); }
-            //当前线程为子线程
+            //当前线程为主线程 直接执行
+            if (IsUIThread()) { action(); }
+            //当前线程为子线程 委托UI线程执行
             else { Application.Current.Dispatcher.Invoke(() => { action(); }); }
+        }
+
+        /// <summary>
+        /// 执行方法 无关UI线程和子线程
+        /// </summary>
+        /// <param name="action">执行的方法</param>
+        public static void ExecuteFunBeginInvoke(Action action)
+        {
+            //当前线程为主线程 直接执行
+            if (IsUIThread()) { action(); }
+            //当前线程为子线程 委托UI线程执行
+            else { Application.Current.Dispatcher.BeginInvoke(() => { action(); }); }
         }
     }
 }
