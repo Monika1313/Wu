d26c789ae7866c8c4a11ca91bbf2ffc895e7e779
Monika <961501261@qq.com>
2022-08-11 11:21:56 +0800

    W

--- Wu.Wpf/Common/Utils.cs
+++ Wu.Wpf/Common/Utils.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Wu.Wpf.Common
@@ -22,6 +18,21 @@ namespace Wu.Wpf.Common
                 return true;
             //不是UI线程
             return false;
+        }
+
+        /// <summary>
+        /// 执行方法 无关UI线程和子线程
+        /// </summary>
+        /// <param name="action">需要执行的方法</param>
+        public static void ExecuteFun(Action action)
+        {
+            //判断是UI线程还是子线程 若是子线程需要用委托
+            var UiThreadId = Application.Current.Dispatcher.Thread.ManagedThreadId;       //UI线程ID
+            var currentThreadId = Environment.CurrentManagedThreadId;                     //当前线程ID
+            //当前线程为主线程
+            if (currentThreadId == UiThreadId) { action(); }
+            //当前线程为子线程
+            else { Application.Current.Dispatcher.Invoke(() => { action(); }); }
         }
     }
 }
