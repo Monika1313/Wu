e30cd0b0c8ac9f9285a58db70e22ffbafb07aa8d
Monika <961501261@qq.com>
2022-12-14 14:02:45 +0800

    W

---
+++ Wu.Wpf/Converters/StringIsNullOrWhiteSpace2Collapsed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace Wu.Wpf.Converters
+{
+    /// <summary>
+    /// string.IsNullOrWhiteSpace   null或空=不可见
+    /// </summary>
+    public class StringIsNullOrWhiteSpace2Collapsed : ValueConverterBase<StringIsNullOrWhiteSpace2Collapsed>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (string.IsNullOrWhiteSpace(value?.ToString()))
+                return Visibility.Collapsed;
+            return Visibility.Visible;
+        }
+    }
+}
--- Wu.Wpf/Converters/StringIsNullOrWhiteSpace2True.cs
+++ Wu.Wpf/Converters/StringIsNullOrWhiteSpace2True.cs
@@ -5,15 +5,15 @@ using System.Windows;
 namespace Wu.Wpf.Converters
 {
     /// <summary>
-    /// string.IsNullOrWhiteSpace   null或空 不可见
+    /// string.IsNullOrWhiteSpace   null或空=true
     /// </summary>
-    public class StringIsNullOrWhiteSpace2Visibility : ValueConverterBase<StringIsNullOrWhiteSpace2Visibility>
+    public class StringIsNullOrWhiteSpace2True : ValueConverterBase<StringIsNullOrWhiteSpace2Visibility>
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (string.IsNullOrWhiteSpace(value?.ToString()))
-                return Visibility.Collapsed;
-            return Visibility.Visible;
+                return true;
+            return false;
         }
     }
 }
