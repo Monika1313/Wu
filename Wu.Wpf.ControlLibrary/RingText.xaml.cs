74a5018eadf5886ca1886d44ce10bbe60b8352a6
weibin wu <961501261@qq.com>
2023-03-26 01:05:49 +0800

    W

--- Wu.Wpf.ControlLibrary/RingText.xaml.cs
+++ Wu.Wpf.ControlLibrary/RingText.xaml.cs
@@ -18,23 +18,20 @@ using System.Windows.Shapes;
 namespace Wu.Wpf.ControlLibrary
 {
     /// <summary>
-    /// RingText.xaml 的交互逻辑
+    /// 圆形环绕文本
     /// </summary>
     public partial class RingText : UserControl
     {
         public RingText()
         {
             InitializeComponent();
-            SetCurrentValue(ForegroundProperty, Brushes.Black);
-            this.SizeChanged += RingText_SizeChanged;
+            this.SizeChanged += RingText_SizeChanged;               //尺寸改变时重绘
         }
 
         private void RingText_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             RefreshLabels();
         }
-
-
 
         #region 依赖属性
         [Category("Wu")]
@@ -46,7 +43,7 @@ namespace Wu.Wpf.ControlLibrary
         }
         public static new readonly DependencyProperty ForegroundProperty =
                     DependencyProperty.Register("Foreground", typeof(Brush), typeof(RingText),
-                        new FrameworkPropertyMetadata(default(Brush), FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender,
+                        new FrameworkPropertyMetadata(Brushes.MediumPurple, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender,
                             new PropertyChangedCallback(OnParamChanged)));
 
         [Category("Wu")]
@@ -59,7 +56,7 @@ namespace Wu.Wpf.ControlLibrary
         public static readonly DependencyProperty CenterToProperty =
                     DependencyProperty.Register("CenterTo", typeof(double), typeof(RingText),
                         new FrameworkPropertyMetadata(0.0,
-                        FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender, 
+                        FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender,
                         new PropertyChangedCallback(OnParamChanged)));
 
         [Category("Wu")]
@@ -71,10 +68,9 @@ namespace Wu.Wpf.ControlLibrary
         }
         public static readonly DependencyProperty TextProperty =
                     DependencyProperty.Register("Text", typeof(string), typeof(RingText),
-                        new FrameworkPropertyMetadata("ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                        new FrameworkPropertyMetadata("123中文abc123中文abc",
                         FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender,
                         new PropertyChangedCallback(OnParamChanged)));
-
 
         [Category("Wu")]
         [Description("尺寸比例")]
@@ -88,7 +84,6 @@ namespace Wu.Wpf.ControlLibrary
                         new FrameworkPropertyMetadata(0.9, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender,
                             new PropertyChangedCallback(OnParamChanged)));
 
-
         [Category("Wu")]
         [Description("字体大小")]
         public new double FontSize
@@ -101,22 +96,35 @@ namespace Wu.Wpf.ControlLibrary
                         new FrameworkPropertyMetadata(18d, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender,
                             new PropertyChangedCallback(OnParamChanged)));
 
-
         [Category("Wu")]
-        [Description("文字间隔角度")]
-        public double AngularSpacing
+        [Description("文字间隔的比例系数 默认1 小于0时文字反序显示")]
+        public double IntervalRatio
         {
-            get { return (double)GetValue(AngularSpacingProperty); }
-            set { SetValue(AngularSpacingProperty, value); }
+            get { return (double)GetValue(IntervalRatioProperty); }
+            set { SetValue(IntervalRatioProperty, value); }
         }
-        public static readonly DependencyProperty AngularSpacingProperty =
-                    DependencyProperty.Register("AngularSpacing", typeof(double), typeof(RingText),
-                        new FrameworkPropertyMetadata(5.0, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender,
+        public static readonly DependencyProperty IntervalRatioProperty =
+                    DependencyProperty.Register("IntervalRatio", typeof(double), typeof(RingText),
+                        new FrameworkPropertyMetadata(1.0, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender,
                             new PropertyChangedCallback(OnParamChanged)));
 
+        [Category("Wu")]
+        [Description("是否在一个圆环上均匀放置文本")]
+        public bool IsARing
+        {
+            get { return (bool)GetValue(IsARingProperty); }
+            set { SetValue(IsARingProperty, value); }
+        }
+        public static readonly DependencyProperty IsARingProperty =
+                    DependencyProperty.Register("IsARing", typeof(bool), typeof(RingText),
+                        new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender,
+                            new PropertyChangedCallback(OnParamChanged)));
 
-
-
+        /// <summary>
+        /// 参数修改时重绘
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="e"></param>
         private static void OnParamChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (d is RingText self)
@@ -140,14 +148,15 @@ namespace Wu.Wpf.ControlLibrary
             //控件宽高
             double width = ActualWidth;
             double height = ActualHeight;
-            if (width <=0 || height <=0)
+            if (width <= 0 || height <= 0)
             {
                 container.Children.Clear();//清空
                 return;
             }
 
             Point ctrlCenter = new(width / 2.0, height / 2.0);//控件中心
-            double angle = CenterTo;
+            double angle = CenterTo;//文字指向圆环的角度
+            double lastAngle = 0;//上一个文字的角度
             double value = 0;
             Point p;
 
@@ -157,14 +166,13 @@ namespace Wu.Wpf.ControlLibrary
             for (int i = 0; i < Text.Length; i++)
             {
                 char item = Text[i];
-                angle += AngularSpacing;//文字指向的角度
                 var txt = new TextBlock
                 {
                     Text = item.ToString(),
                     FontSize = FontSize,
                     Foreground = Foreground,
                     RenderTransformOrigin = new Point(0.5, 0.5), //以文字自身为旋转中心
-                    RenderTransform = new RotateTransform(angle + 90),//文字本身的旋转角度
+                    //RenderTransform = new RotateTransform(angle + 90),//文字本身的旋转角度
                     //Background = Brushes.Black,
                     VerticalAlignment = VerticalAlignment.Center,
                     HorizontalAlignment = HorizontalAlignment.Center,
@@ -174,19 +182,34 @@ namespace Wu.Wpf.ControlLibrary
                 container.Children.Add(txt);//添加进容器
                 txt.UpdateLayout();
 
-                //该字占的角度
-                double an = Math.Atan(txt.ActualWidth / (radius - txt.ActualHeight / 2)) * R2A * 2;
+                if (IsARing)
+                {
+                    angle = 360.0 / count * i;
+                }
+                else
+                {
+                    //该字占的角度
+                    double currentAngle = Math.Atan(txt.ActualWidth / (radius - txt.ActualHeight / 2)) * R2A * 2;
+                    //第一个字不需要旋转自身一般的角度
+                    if (i == 0)
+                    {
+                        angle = CenterTo % 360;//文字指向的角度为起始角度
+                    }
+                    else
+                    {
+                        angle += lastAngle * IntervalRatio / 2 + currentAngle * IntervalRatio / 2;//文字指向的角度 计算当前文字指向的角度
+                    }
+                    lastAngle = currentAngle;//上一个字占的角度
+                }
 
-                //设置在容器中的位置
+                txt.RenderTransform = new RotateTransform(angle + 90);//文字本身的旋转角度
+
+                //设置文本在容器中的位置
                 p = PolarToCartesian(angle, radius);
-
-
                 Canvas.SetLeft(txt, ctrlCenter.X + p.X - txt.ActualWidth / 2.0);
                 Canvas.SetTop(txt, ctrlCenter.Y + p.Y - txt.ActualHeight / 2.0);
             }
         }
-
-
 
         const double R2A = 180.0 / Math.PI;//弧度转角度系数
         const double A2R = Math.PI / 180.0;//角度转弧度系数
@@ -204,7 +227,5 @@ namespace Wu.Wpf.ControlLibrary
             var y = radius * Math.Sin(rad);
             return new Point(x, y);
         }
-
-
     }
 }
