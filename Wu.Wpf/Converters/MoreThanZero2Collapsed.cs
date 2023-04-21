42c55b9a8da6c9b9580ce3d1927c74dd86255e05
Monika <961501261@qq.com>
2023-04-21 17:07:39 +0800

    W

---
+++ Wu.Wpf/Converters/MoreThanZero2Collapsed.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace Wu.Wpf.Converters
+{
+    public class MoreThanZero2Collapsed : ValueConverterBase<MoreThanZero2Collapsed>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && int.TryParse(value.ToString(), out int result))
+            {
+                if (result == 0)
+                {
+                    return Visibility.Visible;
+                }
+            }
+            return Visibility.Collapsed;
+        }
+    }
+}
