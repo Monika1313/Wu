90071d3c4cfaa0b4169e47b90d4c2ce49962e0a5
weibin wu <961501261@qq.com>
2024-08-18 15:49:46 +0800

    💩

--- Wu/Extensions/MathExtension.cs
+++ Wu/Extensions/MathExtension.cs
@@ -5,7 +5,6 @@
 /// </summary>
 public static class MathExtension
 {
-
     public static double Round2(this double value)
     {
         return Math.Round(value, 2);
@@ -15,22 +14,4 @@ public static class MathExtension
     {
         return Math.Round(value, 3);
     }
-
-
-
-    /// <summary>
-    /// 将字符串表示的字节数组 转换为字节数组
-    /// </summary>
-    /// <param name="msg"></param>
-    /// <returns></returns>
-    public static byte[] GetBytes(this string msg)
-    {
-        msg = msg.Replace(" ", "").Replace("-", "");
-        if ((msg.Length % 2) != 0) msg += " ";
-        byte[] returnBytes = new byte[msg.Length / 2];
-        for (int i = 0; i < returnBytes.Length; i++)
-            returnBytes[i] = Convert.ToByte(msg.Substring(i * 2, 2), 16);
-        return returnBytes;
-    }
-
 }
